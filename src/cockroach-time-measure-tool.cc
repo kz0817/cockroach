b6ac46c9b39b7d639ef77a42c802f369b678efb5
Kazuhiro Yamato <kz0817@gmail.com>
2012-12-03 08:50:05 +0900

    added a part of code for list of cockroach-time-measure-tool.

--- src/cockroach-time-measure-tool.cc
+++ src/cockroach-time-measure-tool.cc
@@ -43,7 +43,7 @@ static bool unlock_shm(measured_time_shm_header *header)
 	return false;
 }
 
-bool command_reset(vector<string> &args)
+static bool command_reset(vector<string> &args)
 {
 	static const int FIRST_ALLOC_SHM_SIZE = 1024*1024;
 
@@ -76,7 +76,7 @@ bool command_reset(vector<string> &args)
 	return true;
 }
 
-bool command_info(vector<string> &args)
+static bool command_info(vector<string> &args)
 {
 	int shm_fd = shm_open(COCKROACH_TIME_MEASURE_SHM_NAME, O_RDWR, 0666);
 	if (shm_fd == -1) {
@@ -94,16 +94,26 @@ bool command_info(vector<string> &args)
 
 	if (!lock_shm(header))
 		return false;
-	printf("ver. : %d\n", header->format_version);
-	printf("size : %"PRIu64"\n", header->shm_size);
-	printf("count: %"PRIu64"\n", header->count);
-	printf("index: %"PRIu64"\n", header->next_index);
+	int format_version = header->format_version;
+	uint64_t shm_size = header->shm_size;
+	uint64_t count = header->count;
+	uint64_t next_index = header->next_index;
 	unlock_shm(header);
+
+	printf("ver. : %d\n", format_version);
+	printf("size : %"PRIu64"\n", shm_size);
+	printf("count: %"PRIu64"\n", count);
+	printf("index: %"PRIu64"\n", next_index);
 
 	return true;
 }
 
-void print_usage(void)
+static bool command_list(vector<string> &args)
+{
+	return true;
+}
+
+static void print_usage(void)
 {
 	printf("Usage:\n");
 	printf("\n");
@@ -112,9 +122,9 @@ void print_usage(void)
 	printf("*** Commands ***\n");
 	printf("reset\n");
 	printf("info\n");
+	printf("list\n");
 	printf("\n");
 }
-
 
 int main(int argc, char *argv[])
 {
@@ -122,10 +132,10 @@ int main(int argc, char *argv[])
 		print_usage();
 		return EXIT_SUCCESS;
 	}
-
 	command_map_t command_map;
 	command_map["reset"] = command_reset;
 	command_map["info"] = command_info;
+	command_map["list"] = command_list;
 
 	string command = argv[1];
 	command_map_itr it = command_map.find(command);
