c5bc989e5b81060e99cf28dda0017fec65880e0f
Kazuhiro Yamato <kz0817@gmail.com>
2012-12-07 20:10:43 +0900

    added code to test the measure time.

--- test/testutil.cc
+++ test/testutil.cc
@@ -75,9 +75,10 @@ void testutil::assert_measured_time_lines(int expected_num_line, string &lines)
 
 void testutil::assert_measured_time_format(string &line)
 {
+	static const int NUM_TIME_MEASURE_TOKENS = 5;
 	vector<string> tokens;
-	split(tokens, line, is_any_of(" "), token_compress_on); 
-	cut_fail("%s", __func__);
+	split(tokens, line, is_any_of(" "), token_compress_on);
+	cppcut_assert_equal(NUM_TIME_MEASURE_TOKENS, (int)tokens.size());
 }
 
 string testutil::exec_helper(const char *recipe_path, const char *arg)
