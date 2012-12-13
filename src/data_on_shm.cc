d9ef69db3af6d6cfcaac3e51f0ddfa57dee8e858
Kazuhiro Yamato <kz0817@gmail.com>
2012-12-14 07:51:21 +0900

    added a part of code to record data on shm.

--- src/data_on_shm.cc
+++ src/data_on_shm.cc
@@ -4,6 +4,11 @@
 #include <fcntl.h>
 #include <semaphore.h>
 #include <errno.h>
+#include <unistd.h> 
+#include <sys/types.h>
+
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
 
 #include "cockroach-probe.h"
 #include "utils.h"
@@ -15,8 +20,9 @@
 
 struct primary_header_t {
 	sem_t sem;
-	uint64_t count;
+	uint64_t size;
 	uint64_t head_index;
+	uint64_t count;
 };
 
 struct item_header_t {
@@ -82,13 +88,25 @@ static void open_shm_if_needed(void)
 		return;
 	}
 
-	g_shm_fd = shm_open(COCKROACH_DATA_SHM_NAME, O_RDWR|O_CREAT, 0644);
+	size_t shm_size = sizeof(primary_header_t);
+	g_shm_fd = shm_open(COCKROACH_DATA_SHM_NAME, O_RDWR, 0644);
 	if (g_shm_fd == -1) {
-		ROACH_ERR("Failed to open shm: %d\n", errno);
-		ROACH_ABORT();
+		if (errno == ENOENT) {
+			g_shm_fd = shm_open(COCKROACH_DATA_SHM_NAME,
+			                    O_RDWR|O_CREAT, 0644);
+		}
+		if (g_shm_fd == -1) {
+			ROACH_ERR("Failed to open shm: %d\n", errno);
+			ROACH_ABORT();
+		}
+		if (ftruncate(g_shm_fd, shm_size) == -1) {
+			ROACH_ERR("Failed: ftrunc: %d\n", errno);
+			ROACH_ABORT();
+		}
+		ROACH_INFO("created: SHM for data\n");
 	}
 
-	void *ptr = mmap(NULL, sizeof(primary_header_t),
+	void *ptr = mmap(NULL, shm_size,
 	                 PROT_READ | PROT_WRITE, MAP_SHARED, g_shm_fd, 0);
 	if (ptr == MAP_FAILED) {
 		ROACH_ERR("Failed to map shm: %d\n", errno);
@@ -98,8 +116,9 @@ static void open_shm_if_needed(void)
 	int shared = 1;
 	unsigned int sem_count = 1;
 	sem_init(&g_primary_header->sem, shared, sem_count);
+	g_primary_header->size = shm_size;
+	g_primary_header->head_index = shm_size;
 	g_primary_header->count = 0;
-	g_primary_header->head_index = sizeof(primary_header_t);
 	pthread_mutex_unlock(&g_shm_mutex);
 }
 
@@ -116,7 +135,13 @@ static item_header_t *get_shm_region(size_t size)
 	open_shm_if_needed();
 
 	lock_shm();
+	//uint64_t size = g_primary_header->size;
+	uint64_t shm_size = g_primary_header->size;
 	uint64_t offset = g_primary_header->head_index;
+
+	// truncate shm if its size is small.
+
+	// just reserve region. Writing data is performed later w/o the lock.
 	g_primary_header->head_index += (sizeof(item_header_t) + size);
 	g_primary_header->count++;
 	unlock_shm();
@@ -127,13 +152,27 @@ static item_header_t *get_shm_region(size_t size)
 		return NULL;
 	}
 
+	// make a new mapped region
+	int page_size = utils::get_page_size();
+	uint64_t offset_page_aligned = (offset + page_size - 1) / page_size;
+	offset_page_aligned *= page_size;
+	size_t length = SHM_WINDOW_SIZE;
+	void *ptr = mmap(NULL, length,
+	                 PROT_READ | PROT_WRITE, MAP_SHARED, g_shm_fd,
+	                 offset_page_aligned);
+	if (ptr == MAP_FAILED) {
+		ROACH_ERR("Failed to map shm: %d: offset: %"PRIu64"\n",
+		          errno, offset);
+		ROACH_ABORT();
+	}
+
 	//return (item_header_t *)ptr;
 	return NULL;
 }
 
 static uint8_t *get_shm_data_area_ptr(item_header_t *item_header)
 {
-	return NULL;
+	return (uint8_t *)(item_header + 1);
 }
 
 void cockroach_record_data_on_shm(uint32_t id, size_t size,
