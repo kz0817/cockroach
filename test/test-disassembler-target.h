55bc154ff103b3ea5af0c5b4eedf302db9c1e64a
Kazuhiro Yamato <kz0817@gmail.com>
2013-02-02 22:36:15 +0900

    added a test case: test_mov_Ev_Iz().

---
+++ test/test-disassembler-target.h
@@ -0,0 +1,15 @@
+#ifndef test_disassembler_target_h
+#define test_disassembler_target_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+void target_mov_Ev_Iz(void);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+
+#endif // test_disassembler_target_h
---
+++ test/test-disassembler.cc
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <cppcutter.h>
+
+#include "test-disassembler-target.h"
+#include "disassembler.h"
+
+namespace test_disassember {
+
+opecode *g_ope = NULL;
+
+void setup(void)
+{
+}
+
+void teardown(void)
+{
+	if (g_ope) {
+		delete g_ope;
+		g_ope = NULL;
+	}
+}
+
+// ---------------------------------------------------------------------------
+// Test code
+// ---------------------------------------------------------------------------
+void test_mov_Ev_Iz(void)
+{
+	g_ope = disassembler::parse((uint8_t *)target_mov_Ev_Iz);
+	cppcut_assert_equal(7, g_ope->get_length());
+}
+
+} // namespace test_disassember
