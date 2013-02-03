8c9053a119e3f2d95ca971680086d92e4434fe00
Kazuhiro Yamato <kz0817@gmail.com>
2013-02-03 09:00:03 +0900

    added comment about the target machine code to test_mov_Ev_Iz().

--- test/test-disassembler.cc
+++ test/test-disassembler.cc
@@ -28,6 +28,9 @@ void teardown(void)
 void test_mov_Ev_Iz(void)
 {
 	g_ope = disassembler::parse((uint8_t *)target_mov_Ev_Iz);
+
+	// movl   $0x5,0x18(%rsi)
+	// c7 46 18 05 00 00 00
 	cppcut_assert_equal(7, g_ope->get_length());
 }
 
